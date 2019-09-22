a7aa1980cba1f115b107ac84ae258b0c93f2e995
wangcheng <453451180@qq.com>
2019-09-22 19:55:41 +0800

    聊天程序客户端，当客户端关闭的时候会有一个异常

---
+++ java/com/cdtft/netty/chatserver/ChatClient.java
@@ -0,0 +1,38 @@
+package com.cdtft.netty.chatserver;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 聊天程序客户端
+ *
+ * @author wang.cheng
+ * @date 2019/9/22 12:27
+ * @email 453451180@qq.com
+ **/
+public class ChatClient {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap().group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChatClientInitializer());
+            Channel channel = bootstrap.connect("localhost", 9092).sync().channel();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                channel.writeAndFlush(bufferedReader.readLine() + "\r\n");
+            }
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ java/com/cdtft/netty/chatserver/ChatServerHandler.java
@@ -0,0 +1,63 @@
+package com.cdtft.netty.chatserver;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * 广播处理
+ *
+ * @author wang.cheng
+ * @date 2019/9/21 23:08
+ * @email 453451180@qq.com
+ **/
+public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
+
+    private static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        Channel channel = ctx.channel();
+        channels.forEach(ch -> {
+            if (ch != channel) {
+                ch.writeAndFlush("收到来自" + channel.remoteAddress() + "的消息：" + msg + "\n");
+            } else {
+                ch.writeAndFlush("自己发出消息" + channel.remoteAddress() + "的消息：" + msg + "\n");
+            }
+        });
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channels.writeAndFlush("服务器-" + channel.remoteAddress() + "加入\n");
+        channels.add(channel);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("服务器-" + ctx.channel().remoteAddress() + "加入\n");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("服务器-" + ctx.channel().remoteAddress() + "下线\n");
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        //连接断开netty会自动remove channel
+        System.out.println("服务器-" + channel.remoteAddress() + "断开\n");
+        channels.writeAndFlush("服务器-" + channel.remoteAddress() + "断开\n");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
