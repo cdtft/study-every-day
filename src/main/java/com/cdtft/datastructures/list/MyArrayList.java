a73837a98c128b88ee89322b61f577e2c3851f25
wangcheng <cdtft@qq.com>
2020-05-06 19:09:14 +0800

    add mylist

--- src/main/java/com/cdtft/datastructures/list/MyArrayList.java
+++ src/main/java/com/cdtft/datastructures/list/MyArrayList.java
@@ -7,7 +7,7 @@ public class MyArrayList<T> implements Iterable<T> {
     /**
      * default capacity
      */
-    private static final int DEFAULT_CAPACITY = 3;
+    private static final int DEFAULT_CAPACITY = 10;
 
     /**
      * 当前数组中的元素
@@ -16,6 +16,8 @@ public class MyArrayList<T> implements Iterable<T> {
 
     /**
      * 数组
+     *
+     *
      */
     private Object[] objects;
 
@@ -42,20 +44,17 @@ public class MyArrayList<T> implements Iterable<T> {
     }
 
     public void add(T t) {
-        this.add(size, t);
+        this.set(size++, t);
     }
 
     public void add(int index, T t) {
         if (objects.length == size()) {
             //扩容
-            ensureCapacity(size() * 2 + 1);
+            ensureCapacity(size() * 2);
         }
         //考虑如果index不是插入到队列尾部的时候向后移动位置
-        for(int i = size; i > index; i--) {
-            objects[i] = objects[i - 1];
-        }
-        objects[index] = t;
-        size++;
+
+
     }
 
     private void ensureCapacity(int newCapacity) {
