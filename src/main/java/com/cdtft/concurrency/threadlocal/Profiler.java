8ac018f1757c7d417138079c99c0f1412521177b
wangcheng <cdtft007@gmail.com>
2021-02-05 17:59:42 +0800

    LocalThread

---
+++ src/main/java/com/cdtft/concurrency/threadlocal/Profiler.java
@@ -0,0 +1,31 @@
+package com.cdtft.concurrency.threadlocal;
+
+import com.cdtft.concurrency.threadstate.SleepUtils;
+
+/**
+ * @author: wangcheng
+ * @date: 2021年02月05 17:46
+ */
+public class Profiler {
+
+    private static final ThreadLocal<Long> TIME_THREAD_LOCAL = new ThreadLocal<Long>() {
+        @Override
+        protected Long initialValue() {
+            return System.currentTimeMillis();
+        }
+    };
+
+    public static void start() {
+        TIME_THREAD_LOCAL.set(System.currentTimeMillis());
+    }
+
+    public static Long end() {
+        return System.currentTimeMillis() - TIME_THREAD_LOCAL.get();
+    }
+
+    public static void main(String[] args) {
+        Profiler.start();
+        SleepUtils.second(2);
+        System.out.println(Profiler.end());
+    }
+}
