0b8dd2da77ace3baa1b608009542380bfc01808d
wangcheng <cdtft007@gmail.com>
2021-02-05 14:16:20 +0800

    线程等待通知

--- src/main/java/com/cdtft/concurrency/lock/waitandnotify/WaitNotify.java
+++ src/main/java/com/cdtft/concurrency/lock/waitandnotify/WaitNotify.java
@@ -1,8 +1,14 @@
 package com.cdtft.concurrency.lock.waitandnotify;
 
+import com.cdtft.concurrency.threadstate.SleepUtils;
+
 import java.time.LocalDateTime;
 
 /**
+ *  Monitor.Enter 失败 --> SynchronizedQueue
+ *
+ *  Object.wait() --> WaitQueue --> Object.notify --> SynchronizedQueue
+ *
  * @author : wangcheng
  * @date : 2020年03月31日 14:12
  */
@@ -20,10 +26,10 @@ public class WaitNotify {
                     System.out.println(LocalDateTime.now().toString() + "WaitRunnable get lock...");
                     try {
                         lock.wait();
+                        System.out.println(LocalDateTime.now().toString() + "WaitRunnable 再次被唤醒");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    System.out.println(LocalDateTime.now().toString() + "WaitRunnable 再次被唤醒");
                 }
             }
             System.out.println(LocalDateTime.now().toString() + "WaitRunnable execute success");
@@ -38,26 +44,20 @@ public class WaitNotify {
                 System.out.println(LocalDateTime.now().toString() + "NotifyRunnable get lock...");
                 lock.notify();
                 flag = true;
-                try {
-                    Thread.sleep(5 * 1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                SleepUtils.second(5);
                 System.out.println(LocalDateTime.now().toString() + "NotifyRunnable 释放锁");
             }
+            SleepUtils.second(2);
             synchronized (lock) {
                 System.out.println(LocalDateTime.now().toString() + "NotifyRunnable get lock again");
+                SleepUtils.second(10);
             }
         }
     }
 
     public static void main(String[] args) {
         new Thread(new WaitRunnable()).start();
-        try {
-            Thread.sleep(5 * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        SleepUtils.second(2);
         new Thread(new NotifyRunnable()).start();
     }
 }
