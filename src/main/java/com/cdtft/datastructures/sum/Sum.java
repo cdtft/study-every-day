e9cc4bafb1cec772b2fc77176c786e5238e817c4
wangcheng <cdtft007@gmail.com>
2021-11-22 15:23:46 +0800

    sum

--- src/main/java/com/cdtft/datastructures/sum/Sum.java
+++ src/main/java/com/cdtft/datastructures/sum/Sum.java
@@ -7,15 +7,31 @@ package com.cdtft.datastructures.sum;
 public class Sum {
 
     public static Integer sum(int[] a, int n) {
-         if (n == 1) {
-             return a[0];
-         }
+        if (n == 1) {
+            return a[0];
+        }
         return sum(a, n - 1) + a[n - 1];
     }
 
-    public static void main(String[] args) {
-        int[] a = new int[]{1, 2, 3};
-        System.out.println(sum(a, 3));
+    public static int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
+
+    public static int fib1(int n) {
+        int i = 0;
+        int fn1 = 0;
+        int fn2 = 1;
+        while (i++ < n) {
+            fn2 = fn2 + fn1;
+            //等于上一个fn2
+            fn1 = fn2 - fn1;
+            System.out.println("fn(" + i + ") = " + fn1);
+        }
+        return fn2;
+    }
+
 
 }
---
+++ src/test/java/com/cdtft/datastructures/sum/SumTest.java
@@ -0,0 +1,20 @@
+package com.cdtft.datastructures.sum;
+
+import org.junit.Test;
+
+/**
+ * @author: wangcheng
+ * @date: 2021年11月18 10:49
+ */
+public class SumTest {
+
+    @Test
+    public void fib() {
+        System.out.println(Sum.fib(44));
+    }
+
+    @Test
+    public void fib1() {
+        System.out.println(Sum.fib1(10));
+    }
+}
